576e5babe1cd1b925b55bd0e859a9adb8b938915
Whuichenggong <147113943+Whuichenggong@users.noreply.github.com>
2025-06-02 17:48:22 +0800

    finished: create mux about ph.HandleInitializeProduct

---
+++ internal/router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Zhonghe-zhao/seckill-system/internal/handler"
+)
+
+func SetupRouter(ph *handler.ProductHandler /*sh *handler.SaleHandler*/) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, `{"status":"OK"}`)
+	})
+
+	//手动分组路由 因为net/http不支持分组路由
+	mux.HandleFunc("POST /api/v1/admin/product/initialize", ph.HandleInitializeProduct)
+	//mux.HandleFunc("GET /api/v1/product", ph.HandleGetProductDetails)
+	//mux.HandleFunc("POST /api/v1/sale/attempt", sh.HandleSaleAttempt)
+	return mux
+}
