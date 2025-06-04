415a760b71072f997e2ad92558d767d8f8d7d002
Whuichenggong <147113943+Whuichenggong@users.noreply.github.com>
2025-06-04 21:09:08 +0800

    interface: 重新设计接口为 redis and db

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -2,42 +2,40 @@ package repository
 
 import (
 	"context"
-
 	"github.com/Zhonghe-zhao/seckill-system/internal/model"
+	"github.com/Zhonghe-zhao/seckill-system/internal/repository/db"
+	"github.com/Zhonghe-zhao/seckill-system/internal/repository/rds"
 )
 
-// 负责操作 Redis 中库存
-type StockRepository interface {
+// Redis 商品缓存
+type ProductCache interface {
+	GetProductInfo(ctx context.Context, productID string) (*model.Product, error)
+	SetProductInfo(ctx context.Context, product *model.Product) error
 	GetStock(ctx context.Context, productID string) (int64, error)
 	SetStock(ctx context.Context, productID string, stock int64) error
 	DecrementStock(ctx context.Context, productID string) (int64, error)
-	//DecrementStockLua(ctx context.Context, productID string) (int64, error)
 }
 
-// 负责处理订单相关 Redis 操作（用户是否下单、入队等）
-type OrderRepository interface {
-	AddUserToPurchasedSet(ctx context.Context, productID string, userID string) (bool, error)
-	IsUserInPurchasedSet(ctx context.Context, productID string, userID string) (bool, error)
+// Redis 订单缓存逻辑（是否下过单、入队）
+type OrderCache interface {
+	AddUserToPurchasedSet(ctx context.Context, productID, userID string) (bool, error)
+	IsUserInPurchasedSet(ctx context.Context, productID, userID string) (bool, error)
 	PushOrderToQueue(ctx context.Context, queueName string, orderReq *model.OrderRequest) error
 }
 
-// Redis 缓存商品信息
-type ProductInfoRepository interface {
-	GetProductInfo(ctx context.Context, productID string) (*model.Product, error)
-	SetProductInfo(ctx context.Context, product *model.Product) error
-}
-
-// 数据库操作接口
-type DBRepository interface {
+// DB 商品持久化
+type ProductStorage interface {
 	GetProductByID(ctx context.Context, productID uint) (*model.Product, error)
-	DecrementStock(ctx context.Context, productID uint) error
-	//HasUserOrdered(ctx context.Context, userID uint, productID uint) (bool, error) //后期添加
+	DecrementStockInDB(ctx context.Context, productID uint) error
 	CreateProduct(ctx context.Context, product *model.Product) error
-
-	CreateOrder(ctx context.Context, order *model.Order) error
 }
 
-var _ StockRepository = (*redisRepository)(nil)
-var _ OrderRepository = (*redisRepository)(nil)
-var _ ProductInfoRepository = (*redisRepository)(nil)
-var _ DBRepository = (*dbRepository)(nil)
+// DB 订单持久化
+
+type OrderStorage interface {
+	CreateOrder(ctx context.Context, order *model.Order) error
+	HasUserOrdered(ctx context.Context, userID uint, productID uint) (bool, error)
+}
+
+var _ ProductStorage = (*db.DBRepository)(nil)
+var _ ProductCache = (*rds.RedisRepository)(nil)
